142da4b814e691539701565dd591864acd6e896c
Sonia Huynh <soniahuynhkl@gmail.com>
2024-07-16 23:58:06 +1200

    add title

--- src/App.js
+++ src/App.js
@@ -3,19 +3,22 @@ import "./main.css";
 
 function App() {
   return (
-    <div className="qr-card">
-      <div className="qr-image-container">
-        {/* <img src={soniaQR} alt="Sonia github QR" className="qr-code" /> */}
-        <img src={soniaQR} alt="Sonia github QR" className="qr-code" />
+    <>
+      <h1 id="title">Sonia's Github QR Code</h1>
+      <div className="qr-card">
+        <div className="qr-image-container">
+          {/* <img src={soniaQR} alt="Sonia github QR" className="qr-code" /> */}
+          <img src={soniaQR} alt="Sonia github QR" className="qr-code" />
+        </div>
+        <div className="text-container">
+          <h1>Sonia is a fullstack web developer</h1>
+          <p>
+            Scan the QR code to visit Sonia&apos;s Github and check out her
+            projects
+          </p>
+        </div>
       </div>
-      <div className="text-container">
-        <h1>Sonia is a fullstack web developer</h1>
-        <p>
-          Scan the QR code to visit Sonia&apos;s Github and check out her
-          projects
-        </p>
-      </div>
-    </div>
+    </>
   );
 }
 
