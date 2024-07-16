668d554bc4ccf54620edf64c07e4a142efab6cab
Sonia Huynh <soniahuynhkl@gmail.com>
2024-07-16 22:55:01 +1200

    set up app component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import soniaQR from "./images/sonia-github-qr.png";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">Hello There!</header>
+    <div className="qr-div">
+      <div className="qr-image-div">
+        <img src={soniaQR} alt="Sonia github QR" />
+        <div className="text-container">
+          <p>
+            Scan the QR code to visit Sonia&apos;s Github and check out her
+            projects
+          </p>
+        </div>
+        <h1>Sonia is a fullstack web developer</h1>
+      </div>
     </div>
   );
 }
